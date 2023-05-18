925a5fef014eed2a004149afa021f451efee81c5
Sabari <mailme@isabarirajan.com>
2023-05-19 03:36:51 +0530

    Update PyUtils

--- WebScrapping/TicketScrapper/Main.py
+++ WebScrapping/TicketScrapper/Main.py
@@ -14,67 +14,44 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 # from multiprocessing.pool import ThreadPool
 from os.path import join as joinpath
+from os import getenv
+import sys
+sys.path.append(getenv("PYUTILS_PATH"))
+from Utilities.package import SeleniumUtilities
+from Utilities.package import BS4Utilities
 
-def find_nth_ancestors(element, n):
-    for i in range(n):
-        element = element.find_element(By.XPATH, "./..")
-    return element
-
-def find_nth_sibling(element, n):
-    for i in range(n):
-        element = element.find_element(By.XPATH, ".//following-sibling::*")
-        print(element.tag_name)
-        print(element.get_attribute("tag"))
-    return element
-
-
-def bs4_find_nth_anchestor(element, n):
-    for i in range(n + 1):
-        element = element.parent
-    return element
-
-def bs4_find_nth_sibling(element, n):
-    for i in range(n + 1):
-        element = element.nextSibling
-    return element
-
-def bs4_find_nth_successor(element, n):
-    for i in range(n + 1):
-        element = element.findChildren()[0]
-    return element
-
-pool = ThreadPoolExecutor(10)
-gecko_driver = GeckoDriverManager().install()
-output_folder = "output"
+selenium = SeleniumUtilities(botname="WebScrapper")
+selenium.set_firefox_driver()
+driver = selenium.driver
 base_url = "https://www.stubhub.com/"
-def get_all_events_from_game():
-    url = base_url + "mlb-tickets/grouping/81/"
-    soup = ""
-    # driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    with webdriver.Firefox(service=FirefoxService(gecko_driver)) as driver:
-        driver.get(url)
-        button_locator = "//*[text()='See More']"
-        print("Clicking See More, Until nothing left")
-        while True:
-            try:
-                WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, button_locator)))
-                button = driver.find_element(By.XPATH, button_locator)
-                button.click()
-                # soup = BeautifulSoup(driver.execute_script("return document.body.innerHTML").encode("utf-8").decode("utf-8"), "html.parser")
-                # break
-            except TimeoutException:
-                break
-        
-        time.sleep(5)
-        soup = BeautifulSoup(driver.execute_script("return document.body.innerHTML").encode("utf-8").decode("utf-8"), "html.parser")
+url = base_url + "mlb-tickets/grouping/81/"
+bs4 = BS4Utilities(botname="BS4Scrapper")
 
+def click_seemore_until_nomore():
+    driver.get(url)
+    button_locator = "//*[text()='See More']"
+    print("Clicking See More, Until nothing left")
+    while True:
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, button_locator)))
+            button = driver.find_element(By.XPATH, button_locator)
+            button.click()
+            # soup = BeautifulSoup(driver.execute_script("return document.body.innerHTML").encode("utf-8").decode("utf-8"), "html.parser")
+            # break
+        except TimeoutException:
+            break
+    
+    time.sleep(5)
+    return bs4.generate_bs4(driver.execute_script("return document.body.innerHTML"))
+
+def get_event_links_from_soup(soup):
     events_button = soup.find_all("button")
     for b in events_button:
         if b.text.strip() == "Events":
             events_button = b
             break
-    events_button_anc = bs4_find_nth_anchestor(events_button, 3)
-    event_table = bs4_find_nth_sibling(events_button_anc, 2)
+    events_button_anc = bs4.find_nth_anchestor(events_button, 3)
+    event_table = bs4.find_nth_sibling(events_button_anc, 2)
     all_events = event_table.findChildren("a")
 
     all_events_link = []
@@ -92,45 +69,55 @@ def get_all_events_from_game():
         })
     return all_events_link
 
-def get_seat_data_from_event(event):
-    print("Getting Seat details from : ", event["title"])
-    response = requests.post(event["url"]).json()
-    # with open(joinpath("output", event["title"] + ".json"), "w") as f:
-    #     f.write(json.dumps(response, indent=2))
-    obj_keys = [
-        "Section", "Row", "Price"
-    ]
-    try:
-        with open(joinpath("output", event["title"] + ".csv"), "w") as f:
-            f.write("Section, Row, Price, SeatsRemaining\n")
-            for data in response["Items"]:
-                # print(data)
-                line = ""
-                for key in obj_keys:
-                    val = data[key]
-                    if val == None:
-                        val = " - "
-                    val= val.replace(",", "")
-                    line += val + ", "
-
-                val = data["TicketsLeftInListingMessage"]
-                if val == None:
-                    val = 0
-                else:
-                    val = val["Message"]
-                    if val == None:
-                        val = 0
-                    else:
-                        val = val.split(" ")[0]
-
-                line += str(val) + "\n"
-                f.write(line)
-    except Exception as e:
-        print(e)
+soup = selenium.run_function_within_webdriver(click_seemore_until_nomore)
+all_events_link = get_event_links_from_soup(soup)
+pool = ThreadPoolExecutor(10)
+output_folder = "output"
+print(all_events_link)
 
 
-all_events_link = get_all_events_from_game()
-for event in all_events_link:
-    pool.submit(get_seat_data_from_event, (event))
 
-print("OUT")
+
+
+# def get_seat_data_from_event(event):
+#     print("Getting Seat details from : ", event["title"])
+#     response = requests.post(event["url"]).json()
+#     # with open(joinpath("output", event["title"] + ".json"), "w") as f:
+#     #     f.write(json.dumps(response, indent=2))
+#     obj_keys = [
+#         "Section", "Row", "Price"
+#     ]
+#     try:
+#         with open(joinpath("output", event["title"] + ".csv"), "w") as f:
+#             f.write("Section, Row, Price, SeatsRemaining\n")
+#             for data in response["Items"]:
+#                 # print(data)
+#                 line = ""
+#                 for key in obj_keys:
+#                     val = data[key]
+#                     if val == None:
+#                         val = " - "
+#                     val= val.replace(",", "")
+#                     line += val + ", "
+
+#                 val = data["TicketsLeftInListingMessage"]
+#                 if val == None:
+#                     val = 0
+#                 else:
+#                     val = val["Message"]
+#                     if val == None:
+#                         val = 0
+#                     else:
+#                         val = val.split(" ")[0]
+
+#                 line += str(val) + "\n"
+#                 f.write(line)
+#     except Exception as e:
+#         print(e)
+
+
+# all_events_link = get_all_events_from_game()
+# for event in all_events_link:
+#     pool.submit(get_seat_data_from_event, (event))
+
+# print("OUT")
