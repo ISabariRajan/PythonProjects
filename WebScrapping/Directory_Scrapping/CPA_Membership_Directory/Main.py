5a05a6bcdbb12610bd9a9b2773a365b89a0e7932
Sabari <mailme@isabarirajan.com>
2023-06-13 05:55:06 +0530

    Added New Directory Scrapping Project

--- WebScrapping/Directory_Scrapping/CPA_Membership_Directory/Main.py
+++ WebScrapping/Directory_Scrapping/CPA_Membership_Directory/Main.py
@@ -18,67 +18,107 @@ print(f"{SeleniumUtilities.time_elapsed()} Imports completed")
 class DirectoryScrapping(SeleniumUtilities, BS4Utilities):
 
     records_read = []
+    page_down_times = 0
 
     def __init__(self, **kwargs):
         kwargs["botname"] = "PDFHost.io"
         super().__init__(**kwargs)
         self.print_debug_log("Initialized")
+
+    def make_string_single_line(self, string):
+        return string.replace("\n", " ").replace("\t", " ").replace("  ", " ").strip()
     
-    def capture_details_from_soup(self, soup):
-        parent = soup.find(id="fontevaDetailFields")
-        with open("output.csv", "w") as f:
-            line = ""
-            for child in parent.children:
-                line += child.text + "|"
+    def capture_details_from_soup(self, soup, value):
+        text_set = [value[-2:]]
+        for child in soup.select("span[class='fonteva-slds-text']"):
+            val = self.make_string_single_line(child.text)
+            if (val and  (val != "Billing State/Province")):
+                text_set.append(val)
+
+        with open("output.csv", "a") as f:
+            line = " | ".join(text_set)
             f.write(line + "\n")
-        pass
+
+    def capture_details_from_record(self, record, value, driver):
+        while True:
+            try:
+                self.ActionChains(driver).move_to_element(record).click(record).perform()
+                break
+            except self.exceptions.MoveTargetOutOfBoundsException as e:
+                driver.execute_script("arguments[0].scrollIntoView()", record)
+                self.sleep(5)
+                continue
+            except self.exceptions.StaleElementReferenceException as e:
+                break
+
+        self.WebDriverWait(driver, 100).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "slds-text-heading_medium")))
+        soup = self.convert_webpage_to_soup(driver)
+        
+        thread = Thread(target=self.capture_details_from_soup, args=(soup,value,))
+        thread.daemon = True
+        thread.start()
+        buttons = driver.find_elements(self.By.CLASS_NAME, "slds-button")
+        for button in buttons:
+            if button.get_attribute("innerText") == "Back to Results":
+                self.ActionChains(driver).move_to_element(button).click(button).perform()
+                break
+        self.records_read.append(value)
 
     def click_every_row_in_directory(self, driver):
-        self.WebDriverWait(driver, 10).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "fonteva-record")))
+        self.WebDriverWait(driver, 100).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "fonteva-record")))
         self.sleep(10, "Waiting for PAGE LOAD")
         records = driver.find_elements(self.By.CLASS_NAME, "fonteva-record")
-        if len(records) >= len(self.records_read):
+
+        if len(self.records_read) >= len(records):
             return
         for record in records:
-            value = record.get_attribute("innerText")
+            value = self.make_string_single_line(record.get_attribute("innerText"))
             if value not in self.records_read:
-                self.records_read.append(value)
-                self.ActionChains(driver).move_to_element(record).click(record).perform()
-                self.WebDriverWait(driver, 10).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "slds-text-heading_medium")))
-                soup = self.convert_webpage_to_soup(driver)
-                thread = Thread(target=self.capture_details_from_soup, args=(soup,))
-                thread.daemon = True
-                thread.start()
-                buttons = driver.find_elements(self.By.CLASS_NAME, "slds-button")
-                for button in buttons:
-                    if button.get_attribute("innerText") == "Back to Results":
-                        self.ActionChains(driver).move_to_element(button).click(button).perform()
-                        break
+                print(len(self.records_read), len(records), value)
+                self.click_next_page_until_n(driver)
+                self.capture_details_from_record(record, value, driver)
+                break
             else:
                 continue
         self.click_every_row_in_directory(driver)
 
+    def click_next_page_until_n(self, driver):
+        self.WebDriverWait(driver, 100).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "fonteva-record")))
+        for i in range(1, self.page_no):
+            self.print_debug_log(f"Page: {i}/{self.page_no}")
+            buttons = driver.find_elements(self.By.CSS_SELECTOR, "button.slds-button_icon-bare")
+            for button in buttons:
+                title = button.get_attribute("title")
+                disabled = button.get_attribute("disabled")
+                if title:
+                    if title == "Next Page":
+                        if not disabled:
+                            driver.execute_script("arguments[0].scrollIntoView()", button)
+                            self.sleep(5)
+                            self.ActionChains(driver).move_to_element(button).click(button).perform()
+                            self.records_read = []
+                            return True
+                        else:
+                            return False
+                            break
     def run(self):
-
         url = "https://contractpackaging.my.site.com/LightningMemberPortal/s/searchdirectory?id=a2n4P0000003FHH"
+        driver = ""
         driver = self.get_firefox_driver()
+        self.print_debug_log("Loading Directory Url")
         driver.get(url)
-        self.print_debug_log("TEST")
-        self.WebDriverWait(driver, 10).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "fonteva-record")))
-        self.print_debug_log("TEST")
-        self.click_every_row_in_directory(driver)
-        # records = driver.find_elements(self.By.CLASS_NAME, "fonteva-record")
-        # for record in records:
-        #     record.click()
-        #     self.WebDriverWait(driver, 10).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "slds-text-heading_medium")))
-        #     soup = self.convert_webpage_to_soup(driver)
-        #     name = record.find_element(self.By.CLASS_NAME, "slds-text-heading_medium").get_attribute("innerText")
-        #     print(record)
-        # records = driver.find_elements(self.By.CLASS_NAME, "slds-button")
-        # for record in records:
-        #     print(record.get_attribute("title"), record.get_attribute("disabled"))
-        # .get_attribute("innerHTML")
-        pass
+        self.print_debug_log("Click Every Row in directory Table")
+        can_loop = True
+        self.WebDriverWait(driver, 100).until(self.EC.presence_of_all_elements_located((self.By.CLASS_NAME, "fonteva-record")))
+        max_page = int(driver.find_elements(self.By.CSS_SELECTOR, "span.fonteva-slds-text.slds-m-left--x-small")[1].get_attribute("innerText"))
+        # print(test.get_attribute("innerText"))
+        self.page_no = 1
+        for self.page_no in range(max_page):
+            self.sleep(5)
+            self.click_every_row_in_directory(driver)
+            self.page_no += 1
+            # can_loop = self.click_next_page_until_n(driver, page_no)
+
 
 
 uploadPdf = DirectoryScrapping()
@@ -88,6 +128,3 @@ uploadPdf.find_working_proxies()
 time.sleep(10)
 uploadPdf.set_firefox_driver()
 uploadPdf.run()
-# print(f"Proxies: {len(uploadPdf.proxies)}")
-# time.sleep(10)
-# print(f"Proxies: {len(uploadPdf.proxies)}")
